c1c52fe419e4a8d9d7a1c14d065c703661dde954
Pavel Batygin <killah19935@gmail.com>
2022-05-27 22:50:30 +0300

    feat: add output method
    See below issue in Jira.
    
    Closed: #pb-1

--- src/ru/train/Main.java
+++ src/ru/train/Main.java
@@ -2,7 +2,11 @@ package ru.train;
 
 public class Main {
     public static void main(String[] args) {
+        outPut("Hello");
+    }
 
+    public static void outPut(String string) {
+        System.out.printf(string);
     }
 }
 
